a1c279c9b6675d1ba0f6a0e43a18533cc0926d34
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-25 21:03:03 +0900

    [feat] group recruit list stub (#5)

--- tests/view/schema/group.py
+++ tests/view/schema/group.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from marshmallow import Schema, fields
 
 
@@ -7,3 +9,16 @@ class GroupRegisterResponseSchema(Schema):
 
 class GroupPostRecruitResponseSchema(Schema):
     id: int = fields.Integer(required=True)
+
+
+class _GroupRecruitListResponseSchema(Schema):
+    id: int = fields.Integer(required=True)
+    title: str = fields.String(required=True)
+    description: str = fields.String(required=True)
+    room_name: str = fields.String(required=True)
+
+
+class GroupRecruitListResponseSchema(Schema):
+    recruits: List[_GroupRecruitListResponseSchema] = fields.Nested(
+        _GroupRecruitListResponseSchema, many=True, required=True
+    )
