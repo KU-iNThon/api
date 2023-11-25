0e6d4898f2123aacbe56dc247d1fd3616608cb50
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-26 04:36:11 +0900

    [feat] register

--- fastlib/service/user.py
+++ fastlib/service/user.py
@@ -11,10 +11,20 @@ class UserService:
         self.__session = sessionmaker(bind=engine)
         User.metadata.create_all(engine)
 
-    def login(self, user: User):
+    def login(self, user: User) -> User:
         with self.__session() as session:
             res = session.query(User).filter_by(id=user.id).first()
             if res is None:
                 raise HTTPException(status_code=404, detail="로그인 정보를 찾을 수 없습니다.")
             else:
                 return res
+
+    def register(self, user: User) -> User:
+        with self.__session() as session:
+            if session.query(User).filter_by(id=user.id).first():
+                raise HTTPException(status_code=409, detail="중복된 사용자입니다.")
+            else:
+                session.add(user)
+                res = session.query(User).filter_by(id=user.id).first()
+                session.commit()
+        return res
--- tests/service/test_user.py
+++ tests/service/test_user.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi import HTTPException
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from fastlib.entity.user import User
 from fastlib.service.user import UserService
@@ -16,4 +17,32 @@ def test_login(engine):
     entity = User(nickname="test", id="invalid@com", pw="test-pw")
     with pytest.raises(HTTPException) as e:
         res = service.login(entity)
-        assert e.value.status_code == 404
+    assert e.value.status_code == 404
+
+
+def test_register_ok(engine):
+    service = UserService(engine=engine)
+    entity = User(nickname="test1", id="new10@com", pw="test-pw")
+    res = service.register(entity)
+    # clean
+    with sessionmaker(bind=engine)() as session:
+        session.add_all([res, entity])
+        assert res.id == entity.id
+        session.delete(res)
+        session.delete(entity)
+        session.commit()
+
+
+def test_register_duplicated(engine):
+    service = UserService(engine=engine)
+    entity1 = User(nickname="test2", id="new3@com", pw="test-pw")
+    entity2 = User(nickname="test2", id="new3@com", pw="test-pw")
+    res = service.register(entity1)
+    with pytest.raises(HTTPException) as e:
+        service.register(entity2)
+    assert e.value.status_code == 409
+
+    with sessionmaker(bind=engine)() as session:
+        session.add(res)
+        session.delete(res)
+        session.commit()
